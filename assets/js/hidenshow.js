f31ec9248450a45f6a1886d27c2db212028ae45f
vitorinojoseneto <67804356+vitorinojoseneto@users.noreply.github.com>
2023-06-19 17:32:41 +0100

    first commit

---
+++ assets/js/hidenshow.js
@@ -0,0 +1,120 @@
+
+//HIDE N SHOW OF HEADER -----------------------------------------------------------------------------------------------------------------------
+function brandnews() {
+    var x = document.getElementById("goodnews");
+    if(x.style.display == "none") {
+               x.style.display = "block";
+           }
+        else {
+               x.style.display = "none";
+    }
+}
+function chat() {
+        var x = document.getElementById("talk");
+        if(x.style.display == "none") {
+                   x.style.display = "block";
+               }
+            else {
+                   x.style.display = "none";
+        }
+}
+ 
+function entertain() {
+            var x = document.getElementById("havefun");
+            if(x.style.display == "none") {
+                       x.style.display = "block";
+                   }
+                else {
+                       x.style.display = "none";
+            }
+} 
+function join() {
+            var x = document.getElementById("global");
+            if(x.style.display == "none") {
+                       x.style.display = "block";
+                   }
+                else {
+                       x.style.display = "none";
+            }
+}       
+//HIDE N SHOW OF CONTENT-LEFT----------------------------------------------------------------------------------------------------------------
+function following() {
+    var x = document.getElementById("follow");
+    if(x.style.display == "none") {
+               x.style.display = "block";
+           }
+        else {
+               x.style.display = "none";
+    }
+    }
+
+function market() {
+    var x = document.getElementById("shop");
+    if(x.style.display == "none") {
+               x.style.display = "block";
+           }
+        else {
+               x.style.display = "none";
+    }
+    }
+
+function settings() {
+     var x = document.getElementById("set");
+     if(x.style.display == "none") {
+                x.style.display = "block";
+            }
+         else {
+                x.style.display = "none";
+     }
+     }
+
+function saved() {
+        var x = document.getElementById("save");
+        if(x.style.display == "none") {
+                   x.style.display = "block";
+               }
+            else {
+                   x.style.display = "none";
+        }
+ }
+
+//HIDE N SHOW OF CONTENT-CENTER----------------------------------------------------------------------------------------------------------------
+function react() {
+    var x = document.getElementById("reacts");
+    if(x.style.display == "none") {
+               x.style.display = "block";
+           }
+        else {
+               x.style.display = "none";
+    }
+}
+function share() {
+    var x = document.getElementById("shared");
+    if(x.style.display == "none") {
+               x.style.display = "block";
+           }
+        else {
+               x.style.display = "none";
+    }
+}
+function moreinfo() {
+    var x = document.getElementById("information");
+    if(x.style.display == "none") {
+               x.style.display = "block";
+           }
+        else {
+               x.style.display = "none";
+    }
+}
+//HIDE N SHOW OF CONTENT-RIGHT----------------------------------------------------------------------------------------------------------------
+function mail() {
+    var x = document.getElementById("buy");
+    if(x.style.display == "none") {
+               x.style.display = "block";
+           }
+        else {
+               x.style.display = "none";
+    }
+}
+     /* HIDE N SHOW ,games,users,shopping
+alert("Hello User")*/
